00486b61e7dc655d7fb3525976a8de5c36e35f38
Mehdi Bouallagui <45876988+mbouallagui@users.noreply.github.com>
2025-10-28 18:57:55 +0100

    Revert "Add click action to 'Add ad units' button in app test"
    This reverts commit 5971c079dafedd435d0f0bde7faff0a0a23f0922.

--- test/specs/app.e2e.js
+++ test/specs/app.e2e.js
@@ -4,9 +4,6 @@ describe('Application Test', () => {
     it('should open the app, capture a screenshot, and close the app', async () => {
         // The app is opened automatically when the session starts due to the 'app' capability
 
-        // Click on the 'Add ad units' button
-        await $('~Add ad units').click();
-
         // Capture a screenshot
         const screenshotPath = path.join(process.cwd(), 'app_screenshot.png');
         await browser.saveScreenshot(screenshotPath);
