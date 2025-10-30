4bb988346ed561ade6fc3fada10f8bfaa48e00db
Mehdi Bouallagui <45876988+mbouallagui@users.noreply.github.com>
2025-10-30 10:47:55 +0100

    Remove all files

--- test/specs/example.e2e.js
+++
@@ -1,6 +0,0 @@
-describe('BrowserStack website', () => {
-    it('should have the correct title', async () => {
-        await browser.url('https://www.browserstack.com/');
-        await expect(browser).toHaveTitle('BrowserStack | #1 Test Infrastructure for Instant, On-Demand Testing');
-    });
-});
